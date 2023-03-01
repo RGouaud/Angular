26aa331a1ab5f5d0c6869eb80192a2f241f01af0
RomainG <rgouaud@iutbayonne.univ-pau.fr>
2023-03-01 08:43:29 +0100

    Ajout d'un component header, création de la classe CD et ajout des attributs

---
+++ src/models/cd.ts
@@ -0,0 +1,12 @@
+export class Cd {
+    id!:number;
+    title!:string;
+    author!:string;
+    price!:number;
+    thumbnail!:string;
+    dateRelease!:Date;
+    quantity!:number;
+    onSale?:boolean;
+    
+    
+}
